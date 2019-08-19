3f74c13af3cf47ee9869206a59f74a5196c0622d
murphylee2048 <1134697131@qq.com>
2019-08-19 20:26:09 +0800

    test: should_return_2_when_updateQuality_given_name_sulfuras_and_quality_2

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -18,9 +18,18 @@ public class GildedRoseTest {
         assertEquals(1, items[0].quality);
     }
 
+    @Test
+    public void should_return_2_when_updateQuality_given_name_sulfuras_and_quality_2() {
+        Item[] items = {new Item("Sulfuras, Hand of Ragnaros", 1, 2)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(2, items[0].quality);
+    }
 
     @Test
-    public void should_return_1_when_updateQuality_given_name_other_and_quality_0() {
+    public void should_return_0_when_updateQuality_given_name_other_and_quality_0() {
         Item[] items = {new Item("other", 1, 0)};
         GildedRose gildedRose = new GildedRose(items);
 
